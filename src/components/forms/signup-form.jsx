e60be10b6dc16f19eaf4fe9d01904217b0bad203
Jhon-crypt <johnoladele690@gmail.com>
2025-02-08 08:06:31 +0100

    feat: Added signup form

--- src/components/forms/signup-form.jsx
+++ src/components/forms/signup-form.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/no-unknown-property */
 import { Link } from "react-router-dom"
-export default function SigninForm() {
+export default function SignupForm() {
     return (
         <>
+            <div class="form-floating">
+                <div class="input-group">
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                </div>
+            </div>
             <div class="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
                 <label for="floatingInput">Email address</label>
@@ -15,11 +21,11 @@ export default function SigninForm() {
             <div class="form-check text-start my-3">
                 <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
                 <label class="form-check-label" for="flexCheckDefault">
-                    Remember me
+                    Agree to terms and conditions
                 </label>
             </div>
             <button class="btn w-100 py-2" type="submit" style={{ "backgroundColor": "#2466FF", "color": "white", "borderRadius": "20px" }}>Sign in</button>
-            <Link to="/signup"><small>Sign up here</small></Link>
+            <Link to="/signin"><small>Sign in here</small></Link>
         </>
     )
 }
